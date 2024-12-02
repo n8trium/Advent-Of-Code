759de7a0e334e992efeb8ef3c2ac4a3d78911d32
n8trium <nathan-loutsch@uiowa.edu>
2024-12-02 12:07:13 -0600

    added part 1 of day 1

--- 2024/01/1.py
+++ 2024/01/1.py
@@ -2,13 +2,17 @@
 # Day 1
 # Part 1
 
-total = 0
+total = 0 # our total subtracted from each list
+b=[] # first list
+c=[] # second list
 
-with open(input.txt) as lines:
-    for line in lines:
-        a = line.split(" ")
-        b = a[0]
-        c = a[1]
-        total +=abs(b-c)
-
-print(total)
+with open("input.txt") as lines:
+    for line in lines: # iterate through each file
+        a = line.split("   ") # splits into two lists: b & c
+        b.append(int(a[0])) # makes list b
+        c.append(int(a[1])) # makes list c
+b.sort() # sort in ascending manner
+c.sort() # sort in ascending manner
+for i in range(len(b)): # iterate over b
+    total+=(abs(b[i]-c[i])) # add each subtracted absolute value to our total
+print(total) # print the total
