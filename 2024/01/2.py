432787cb5144352ff9ce153d1a7af08279d7179f
n8trium <nathan-loutsch@uiowa.edu>
2024-12-02 13:39:13 -0600

    added part 2

---
+++ 2024/01/2.py
@@ -0,0 +1,19 @@
+# Advent of code
+# Day 1
+# Part 1
+
+total = 0 # our total subtracted from each list
+b=[] # first list
+c=[] # second list
+
+with open("input.txt") as lines:
+    for line in lines: # iterate through each file
+        a = line.split("   ") # splits into two lists: b & c
+        b.append(int(a[0])) # makes list b
+        c.append(int(a[1])) # makes list c
+
+for i in range(len(b)): # iterate over b
+    for j in range(len(c)): # iterate over c
+        if b[i]==c[j]: # compare numbers
+            total+=(c[j]) # add each value to the similarity total
+print(total) # print the total
