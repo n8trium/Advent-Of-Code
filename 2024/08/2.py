5125ba931d36f4ccd03acd5360596aa58691fe33
n8trium <n.loutsch@gmail.com>
2024-12-08 01:40:57 -0600

    8-2

---
+++ 2024/08/2.py
@@ -0,0 +1,59 @@
+import re
+
+towers={}
+antinodes=[]
+data = [line.strip() for line in open('example.txt')]
+x_range=len(data[0])
+y_range=len(data)
+print(x_range, y_range)
+hash_map=[]
+blank=''
+for x in range(x_range):
+    blank=blank+('.')
+for y in range(y_range):
+    hash_map.append(blank)
+print(hash_map)
+
+def moving(coords_i, coords_f, x_max, y_max):
+    nodes=[]
+    while ?
+    list.append()
+
+
+for y in range(len(data)):
+    print(data[y])
+    for x in range(len(data[y])):
+        if re.match('\w', data[y][x]) != None:
+            match = re.search('\w', data[y][x])
+            if match:
+                towers.setdefault(match.group(0), []).append((x, y))
+print(towers)
+for freq in towers:
+    print(f'\n{freq}')
+    for first in towers[freq]:
+        for second in towers[freq]:
+            if second != first:
+                print(f'1: {first} 2: {second}')
+                delta_x=second[0]-first[0]
+                delta_y=second[1]-first[1]
+                positive=(second[0]+delta_x, second[0]+delta_y)
+                negative=(first[0]-delta_x, first[0]-delta_y)
+                if 0<=positive[0]<x_range and 0<=positive[1]<y_range:
+                    antinodes.append(positive)
+                negative=(2*first[0]-second[0], 2*first[1]-second[1])
+                if 0<=negative[0]<x_range and 0<=negative[1]<y_range:
+                    antinodes.append(negative)
+                print(positive, negative)
+            else:
+                break
+for i in range(len(antinodes)):
+    print(antinodes[i][1], antinodes[i][0])
+    hash_map[antinodes[i][1]]=hash_map[antinodes[i][1]][:antinodes[i][0]]+'#'+hash_map[antinodes[i][1]][1+antinodes[i][0]:]
+print(sorted(antinodes))
+total=0
+for y_1 in range(len(hash_map)):
+    for x_1 in range(len(hash_map)):
+        if hash_map[y_1][x_1]=='#':
+            total+=1
+    print(hash_map[y_1])
+print(total)
