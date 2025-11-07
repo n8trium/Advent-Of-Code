d91d74f43ed67935e2bd7402c813b0d810ce8082
n8trium <nathan-loutsch@uiowa.edu>
2025-11-07 13:12:20 -0600

    New Test

--- 2023/09/part2.py
+++ 2023/09/part2.py
@@ -5,30 +5,25 @@ with open("input.txt") as fp:
 
 def recurser(numline, number):
     line2=[]
-    print(numline)
     for i in range(len(numline)-1):
         b=numline[i+1]-numline[i]
         line2.append(b)
     k=0
-    number-=line2[0]
-    print(number)
+    number+=line2[-1]
     for j in line2:
         if j==0:
             k+=1
     if k==len(line2):
-        print(f"k: {k}")
-        print(number)
         return number
     else:
-        print(f"Recursion Here!, {k}")
         return recurser(line2, number)
 
 for line in input:
     array=line.strip().split(" ")
     for i in range(len(array)):
         array[i]=int(array[i])
-    perline = recurser(array, array[0])
-    print(f"perline: {perline}")
+    perline = recurser(array, array[-1])
+    print(f"Value: {perline}")
     total+=perline
 
 print(f"Total: {total}")
